d4b790eb6fbd6f8918f90e4cdfd77a7a3a8d7f17
Felix Hernandez <fa18.fhmail.com>
2021-11-15 14:07:41 -0600

    Registro del usuario (correo y contraseña) en firebase

--- app/src/main/java/com/example/twitteracademico/MainActivity.java
+++ app/src/main/java/com/example/twitteracademico/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
 
     TextView mTextViewRegister;
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    Button mButtonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +26,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextViewRegister = findViewById(R.id.textViewRegister);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mButtonLogin = findViewById(R.id.btnLogin);
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
         //Navegacion a la pantalla de registro
         mTextViewRegister.setOnClickListener(new View.OnClickListener() {
@@ -28,4 +46,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void login(){
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+
+        Log.d("Campo", "email: "+email);
+        Log.d("Campo", "password: "+password);
+
+    }
+
 }
--- app/src/main/java/com/example/twitteracademico/RegisterActivity.java
+++ app/src/main/java/com/example/twitteracademico/RegisterActivity.java
@@ -1,15 +1,33 @@
 package com.example.twitteracademico;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
 
     CircleImageView mCircleImageViewBack;
+    TextInputEditText mTextInputUsername;
+    TextInputEditText mTextInputEmail;
+    TextInputEditText mTextInputPassword;
+    TextInputEditText mTextInputConfirmPassword;
+    Button mButtonRegister;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,20 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         mCircleImageViewBack = findViewById(R.id.circleImageBack);
+        mTextInputEmail = findViewById(R.id.textInputEmail);
+        mTextInputUsername = findViewById(R.id.textInputUsername);
+        mTextInputPassword = findViewById(R.id.textInputPassword);
+        mTextInputConfirmPassword = findViewById(R.id.textInputConfirmPassword);
+        mButtonRegister = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
+
+        mButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
 
         mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,4 +58,53 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
     }
+
+    private void register(){
+        String username = mTextInputUsername.getText().toString();
+        String email = mTextInputEmail.getText().toString();
+        String password = mTextInputPassword.getText().toString();
+        String confirmPassword = mTextInputConfirmPassword.getText().toString();
+
+        if(!username.isEmpty() && !email.isEmpty() && !password.isEmpty() && !confirmPassword.isEmpty()){
+            if(isEmailValid(email)){
+                if(password.equals(confirmPassword)){
+                    if(password.length()>=6){
+                        createUser(email,password);
+                    }else{
+                        Toast.makeText(this, "La contraseña debe de tener al menos 6 caracteres", Toast.LENGTH_LONG).show();
+                    }
+
+                }else{
+                    Toast.makeText(this, "Las contraseñas ingresadas no son iguales", Toast.LENGTH_LONG).show();
+                }
+            }else{
+                Toast.makeText(this, "El email ingresado no es valido", Toast.LENGTH_LONG).show();
+            }
+
+        }else{
+            Toast.makeText(this, "Uno o mas campos esta vacio", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void createUser(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(RegisterActivity.this, "Registro Exitoso", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(RegisterActivity.this, "No se pudo registrar el usuario", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    //Validación de email valido
+    public boolean isEmailValid(String email){
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
 }
