30253f2f57b68fd7986b97679af0f3ea5467c9f0
Felix Hernandez <fa18.fhmail.com>
2021-11-14 20:40:50 -0600

    Navegacion de la venta de registro al menu de login

--- app/src/main/java/com/example/twitteracademico/RegisterActivity.java
+++ app/src/main/java/com/example/twitteracademico/RegisterActivity.java
@@ -3,13 +3,27 @@ package com.example.twitteracademico;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    CircleImageView mCircleImageViewBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mCircleImageViewBack = findViewById(R.id.circleImageBack);
+
+        mCircleImageViewBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
